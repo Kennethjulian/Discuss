2b228d0943da716d8e71881a199c7495a98b3307
Kenneth Julian <kennethjulian026@gmail.com>
2019-12-10 13:50:12 -0600

    added topics form

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -1,5 +1,15 @@
 defmodule Discuss.TopicController do
-    def new() do
+    use Discuss.Web, :controller # makes web.ex modules availible here
+    
+    alias Discuss.Topic
+
+    def new(conn, _params) do
+        changeset = Topic.changeset(%Topic{}, %{})
+
+        render conn, "new.html", changeset: changeset
+    end
+
+    def create(conn, %{"topic" => topic}) do
         
     end
 end
